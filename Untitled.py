712bba0d66ccb3e87d543524f21392e005d02a5b
Chetyh <43051703+Chetyh@users.noreply.github.com>
2018-12-31 05:47:04 +0800

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[166]:
+
+
+import numpy as np
+A=np.array([[1,2,3],[4,5,6]])
+
+
+# ## Task 1
+# Using the equation given in the overview, classify all the test data and calculate the percentage of correctly classified digits. Report your percentages in your Google Document and comment on how well (or poorly) it does.
